1a03b51ae83250dd7f09e574eafa2cf02e9ea361
Ben Jiron <benjiron@Bens-MacBook-Pro.local>
2022-12-14 15:11:49 -0700

    write to stdout/stderr

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::io::{self, Write};
+
 #[tokio::main]
 async fn main() {
+  let mut stdout = io::stdout().lock();
+  let mut stderr = io::stderr().lock();
   match polymesh_cli::run().await {
-    Ok(res) => println!("{}", res),
+    Ok(res) => write!(stdout, "{}", res).expect("failed to write to stdout"),
     Err(e) => {
-      eprintln!("{}", e);
+      write!(stderr, "{}", e).expect("failed to write to stderr");
       std::process::exit(1);
     }
   }
+  stdout.flush().expect("failed to flush stdout");
+  stderr.flush().expect("failed to flush stdout");
 }
